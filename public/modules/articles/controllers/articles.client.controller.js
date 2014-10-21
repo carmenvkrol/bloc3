a53476dd86e3725d78a854264fe396eb6448d738
Carmen V Krol <carmen.krol@gmail.com>
2014-10-20 22:42:12 -0400

    findOneTag function work for Phil

--- public/modules/articles/controllers/articles.client.controller.js
+++ public/modules/articles/controllers/articles.client.controller.js
@@ -2,6 +2,13 @@
 
 angular.module('articles').controller('ArticlesController', ['$http', '$scope', '$stateParams', '$location', '$timeout', 'Authentication', 'Articles',
 	function($http, $scope, $stateParams, $location, $timeout, Authentication, Articles) {
+		var tagOnLoad;
+		var tagSelected;
+
+		$scope.tagOnLoad = true;
+
+		$scope.tagSelected = false;
+
 		$scope.authentication = Authentication;
 
 		$scope.create = function() {
@@ -72,7 +79,72 @@ angular.module('articles').controller('ArticlesController', ['$http', '$scope', 
            });
 
         $scope.tags = {};
-        console.log($scope.tags);
 		};
+
+		$scope.findOneTag = function() {
+			$scope.find().then(function (data) {
+				$scope.articles2 = [];
+				data.map(function (article) {
+					var tags = article.tags.map(function (tag) {
+						return tag.text;
+					});
+					
+					article.tagsReduced = tags;
+
+					if (article.tagsReduced.indexOf('Tag-4') !== -1) {
+						$scope.articles.push(article);
+						console.log(article);
+					}
+				});
+				
+				console.log($scope.articles2);
+				
+				
+				// for (var i=0; i<$scope.articles.length; i++) {
+				// 	for (var j=0; j<$scope.articles[i].tags.length; j++) {
+				// 		if ($scope.articles[i].tags[j].text === 'Tag-4'){
+				// 			$scope.tagSelected = true;
+				// 		}
+				// 	}
+				// }
+				// return $scope.tagSelected;
+			});
+		};
+		// 	$http
+		// 		.get('/articles')
+		// 		.success(function(data){
+		// 			$timeout(function(){
+		// 				$scope.tagOnLoad = false;
+		// 				// $scope.articles = data;
+		// 				$scope.articles2 = [];
+		// 				data.map(function (article) {
+		// 					var tags = article.tags.map(function (tag) {
+		// 						return tag.text;
+		// 					});
+							
+		// 					article.tagsReduced = tags;
+
+		// 					if (article.tagsReduced.indexOf('Tag-4') !== -1) {
+		// 						$scope.articles.push(article);
+		// 						console.log(article);
+		// 					}
+		// 				});
+						
+		// 				console.log($scope.articles2);
+						
+						
+		// 				// for (var i=0; i<$scope.articles.length; i++) {
+		// 				// 	for (var j=0; j<$scope.articles[i].tags.length; j++) {
+		// 				// 		if ($scope.articles[i].tags[j].text === 'Tag-4'){
+		// 				// 			$scope.tagSelected = true;
+		// 				// 		}
+		// 				// 	}
+		// 				// }
+		// 				return $scope.tagSelected;
+		// 			});
+		// 		})
+		// 		.error(function(){
+		// 		});
+		// };
 	}
 ]);
