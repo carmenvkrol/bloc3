d8dd3e11acee0639f99a17b248bc03ec60a5f13a
Carmen V Krol <carmen.krol@gmail.com>
2014-10-29 19:33:42 -0400

    delete tags functionality

--- public/modules/articles/config/articles.client.routes.js
+++ public/modules/articles/config/articles.client.routes.js
@@ -5,6 +5,10 @@ angular.module('articles').config(['$stateProvider',
 	function($stateProvider) {
 		// Articles state routing
 		$stateProvider.
+		state('tags', {
+			url: '/tags',
+			templateUrl: 'modules/articles/views/tags.client.view.html'
+		}).
 		state('listArticles', {
 			url: '/articles',
 			templateUrl: 'modules/articles/views/list-articles.client.view.html'
---
+++ public/modules/articles/controllers/tags.client.controller.js
@@ -0,0 +1,61 @@
+'use strict';
+
+angular.module('articles').controller('TagsController', ['$http', '$scope', '$stateParams', '$location', '$timeout', '$resource', 'Authentication', 'Articles',
+	function($http, $scope, $stateParams, $location, $timeout, $resource, Authentication, Articles) {
+
+    var articles = []; 
+		
+    //MAKE THIS INTO SERVICE IF KEEP USING
+    $scope.findTags = function() {
+       $http
+          .get('/article_tags')
+          .success(function(data){
+              $timeout(function() {
+                $scope.tags = data;
+              });
+           })
+           .error(function(){
+           });
+
+        $scope.tags = {};
+    };
+
+    $scope.getArticles = function() {
+      $http
+        .get('/articles')
+        .success(function(data){
+          $timeout(function(){
+            articles = data;
+          });
+        })
+        .error(function(){
+        }); 
+    };
+
+    $scope.updateArticle = function(article) {
+      $http
+        .put('/articles/' + article._id, article)
+        .success(function(article){
+            article;
+        });
+    };
+
+    $scope.deleteTag = function(tag) {
+
+        $scope.getArticles();
+
+        angular.forEach(articles, function(article){
+            for (var i=0; i<article.tags.length; i++) {
+              if (tag === article.tags[i].text) {
+                article.tags.splice(i,1);
+                $scope.updateArticle(article);
+              }
+            }
+
+            $scope.findTags();
+
+        });
+
+      };
+	}
+]);
