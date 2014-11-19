2a9f5c44cde7315c7a00d8f74438167fe624ba9f
Phil Spitler (PRS) <prs@sproutkey.com>
2014-11-18 23:41:31 -0500

    cleaned up dealing with tags

--- public/modules/articles/controllers/tags.client.controller.js
+++ public/modules/articles/controllers/tags.client.controller.js
@@ -4,18 +4,19 @@ angular.module('articles').controller('TagsController', ['$http', '$scope', '$st
 	function($http, $scope, $stateParams, $location, $timeout, $resource, $q, Authentication, Articles) {
 
     $scope.articles = [];
+    $scope.tags = {};
 
     //MAKE THIS INTO SERVICE IF KEEP USING
     $scope.findTags = function() {
        $http
           .get('/article_tags')
           .success(function(data){
+            console.log(data);
               $scope.tags = data;
            })
            .error(function(){
            });
 
-        $scope.tags = [];
     };
 
     $scope.getArticles = function() {
@@ -29,51 +30,24 @@ angular.module('articles').controller('TagsController', ['$http', '$scope', '$st
         });
     };
 
-    $scope.updateArticle = function(article) {
-      return $http.put('/articles/' + article._id, article).success(function(article){
-        $scope.article = article;
-      });
+    $scope.updateTag = function(theTag) {
+       $http
+          .post('/article_tags', theTag)
+          .success(function(data){
+            $scope.tags = data;
+           })
+           .error(function(){
+           });
     };
 
-    $scope.updateTag = function(key) {
-      var val = this.val;
-      var updateKey = this.key;
-      var oldKey = this.val.original;
-      var promise = $scope.getArticles();
-
-      promise.then(function(){
-        angular.forEach($scope.articles, function(article){
-          for (var i=0; i < val.bookmarks.length; i++) {
-            if (val.bookmarks[i] === article._id) {
-              for (var j=0; j<article.tags.length; j++) {
-                if (article.tags[j].text === oldKey) {
-                  article.tags[j].text = updateKey;
-                }
-                $scope.updateArticle(article);
-              }
-            }
-          }
-        });
-      });
-    };
-
-    $scope.deleteTag = function(tag) {
-        var updateCalls = [];
-        var promise = $scope.getArticles();
-
-        promise.then(function() {
-          angular.forEach($scope.articles, function(article){
-            for (var i=0; i<article.tags.length; i++) {
-              if (tag === article.tags[i].text) {
-                article.tags.splice(i,1);
-                updateCalls.push($scope.updateArticle(article));
-              }
-            }
-          });
-          $q.all(updateCalls).then(function () {
-            $scope.findTags();
-          });
-        });
+    $scope.deleteTag = function(theTag) {
+       $http
+          .delete('/article_tags/' + theTag)
+          .success(function(data){
+            $scope.tags = data;
+           })
+           .error(function(){
+           });
     };
 
     $scope.showMessage = function(message) {
