4ec51b69b649a1c4db6d5c9f8e1c2b67091d442f
Carmen V Krol <carmen.krol@gmail.com>
2014-11-24 12:09:54 -0500

    mongodb production

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var process.env.MONGOHQ_URL="mongodb://"+process.env.MONGOHQ_UN+":"+process.env.MONGOHQ_PW+"@dogen.mongohq.com:10026/app31834364";
+
 module.exports = {
   db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/bloc3',
   assets: {
