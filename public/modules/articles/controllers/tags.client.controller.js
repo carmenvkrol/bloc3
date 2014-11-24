1e08e9bfd55c2601b0e26f1bf0a36c12bd82be3a
Phil Spitler (PRS) <prs@sproutkey.com>
2014-11-24 16:57:44 -0500

    basically turned controller into a proxy for the service

--- public/modules/articles/controllers/tags.client.controller.js
+++ public/modules/articles/controllers/tags.client.controller.js
@@ -8,31 +8,13 @@ var TagsController = (function () {
     this.message = '';
   }
 
-  TagsController.prototype.findTags = function() {
+  TagsController.prototype.tagService = function (operation, args, message) {
     var vm = this;
-    this.TagService.findTags().then(function (response) {
-      vm.tags = response.data;
-    });
-  };
 
-  TagsController.prototype.updateTag = function(theTag, message) {
-    var vm = this;
-    this.TagService.updateTag(theTag).then(function (response) {
+    this.TagService[operation](args).then(function (response) {
       vm.tags = response.data;
       vm.message = message;
     });
-  };
-
-  TagsController.prototype.deleteTag = function(theTag, message) {
-    var vm = this;
-    this.TagService.deleteTag(theTag).then(function (response) {
-      vm.tags = response.data;
-      vm.message = message;
-    });
-  };
-
-  TagsController.prototype.displayMessage = function() {
-    return this.message.length > 0;
   };
 
   TagsController.$inject = ['TagService'];
