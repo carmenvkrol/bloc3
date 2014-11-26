b7d83022c528637cff1111b0242ead1e9424e7c4
Carmen V Krol <carmen.krol@gmail.com>
2014-11-25 19:12:56 -0500

    modified application file

--- public/dist/application.js
+++ public/dist/application.js
@@ -1,21 +1,4 @@
 'use strict';
-//Start by defining the main module and adding the module dependencies
-angular.module(ApplicationConfiguration.applicationModuleName, ApplicationConfiguration.applicationModuleVendorDependencies);
-// Setting HTML5 Location Mode
-angular.module(ApplicationConfiguration.applicationModuleName).config([
-  '$locationProvider',
-  function ($locationProvider) {
-    $locationProvider.hashPrefix('!');
-  }
-]);
-//Then define the init function for starting up the application
-angular.element(document).ready(function () {
-  //Fixing facebook bug with redirect
-  if (window.location.hash === '#_=_')
-    window.location.hash = '#!';
-  //Then init the app
-  angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);
-});'use strict';
 // Init the application configuration module for AngularJS application
 var ApplicationConfiguration = function () {
     // Init module configuration options
@@ -43,7 +26,24 @@ var ApplicationConfiguration = function () {
       applicationModuleVendorDependencies: applicationModuleVendorDependencies,
       registerModule: registerModule
     };
-  }();'use strict';
+  }();
+//Start by defining the main module and adding the module dependencies
+angular.module(ApplicationConfiguration.applicationModuleName, ApplicationConfiguration.applicationModuleVendorDependencies);
+// Setting HTML5 Location Mode
+angular.module(ApplicationConfiguration.applicationModuleName).config([
+  '$locationProvider',
+  function ($locationProvider) {
+    $locationProvider.hashPrefix('!');
+  }
+]);
+//Then define the init function for starting up the application
+angular.element(document).ready(function () {
+  //Fixing facebook bug with redirect
+  if (window.location.hash === '#_=_')
+    window.location.hash = '#!';
+  //Then init the app
+  angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);
+});
 // Use Applicaion configuration module to register a new module
 ApplicationConfiguration.registerModule('articles');'use strict';
 // Use Applicaion configuration module to register a new module
